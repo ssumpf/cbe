28df9ffa1191362e0210ecb320541d2dce153220
Josef Söntgen <josef.soentgen@genode-labs.com>
2019-04-01 18:47:36 +0200

    aes_cbc_block: initial commit

---
+++ src/server/aes_cbc_block/cbe.ads
@@ -0,0 +1,17 @@
+--
+-- Copyright (C) 2019 Genode Labs GmbH
+--
+-- This file is part of the Genode OS framework, which is distributed
+-- under the terms of the GNU Affero General Public License version 3.
+--
+
+pragma Ada_2012;
+
+package CBE
+with Spark_Mode
+is
+	pragma Pure;
+
+	type Number_Of_Primitives_Type is mod 2**64;
+
+end CBE;
